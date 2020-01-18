f5c3aea1370c5687183f1f24db8bd27aff326f95
BottaDev <fedebotta@gmail.com>
2020-01-17 23:56:16 -0300

    Added Parallax Test

---
+++ BlackSpace/Assets/Script/ScrollUV.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollUV : MonoBehaviour
+{
+    public float scrollSpeed = 1f;   // CUIDADO
+
+    void Update()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+
+        Material mat = mr.material;
+
+        Vector2 offset = mat.mainTextureOffset;
+
+        offset.x += Time.deltaTime / scrollSpeed;
+
+        mat.mainTextureOffset = offset;
+    }
+}
